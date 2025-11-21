010b308c9e63dd02c558de34c0c201e78ab741cd
SamFTW0128 <140744153+SamFTW0128@users.noreply.github.com>
2025-11-22 02:48:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // frontend/config.js
 // During local dev:
-const API_BASE = 'http://localhost:3000';
+const API_BASE = 'https://newestfoodtrackingbackend.onrender.com';
 // After deploy, change to e.g.
 // const API_BASE = 'https://cottonwar-backend.onrender.com';
+
